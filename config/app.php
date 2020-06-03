c8803728b22f03b169fc2428d45f1b651cf055e8
Ahmed Ali Qureshi <55103689+AhmedQureshi131@users.noreply.github.com>
2020-06-03 16:45:50 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -119,9 +119,9 @@ return [
     |
     */
 
-    'key' => env('APP_KEY','SomeRandomString'),
+    'key' => env('APP_KEY'),
 
-    'cipher' => 'AES-128-CBC',
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
